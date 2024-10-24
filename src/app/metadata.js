48d493956ce64644ec685a29b9f0cd04451f0613
Santiago Rodriguez Bermudez <srba87@gmail.com>
2024-10-23 21:44:27 -0500

    improve metadata

--- src/app/metadata.js
+++ src/app/metadata.js
@@ -1,4 +1,4 @@
-export const metadata = {
+export default metadata = {
     title: 'Willow Lane Acquisition Corporation | Home',
     description: 'Willow Lane Acquisition Corporation is a blank check company formed for the purpose of effecting a merger, amalgamation, share exchange, asset acquisition, share purchase, reorganization or similar business combination.',
   }
