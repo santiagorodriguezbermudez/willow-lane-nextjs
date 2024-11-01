39718a6e0a0829b53324526a015767a850edd694
Santiago Rodriguez Bermudez <santiago@exante.app>
2024-11-01 09:31:59 -0500

    Fix layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,6 @@ const NavItems = ({ mobile }) => (
     <li><Link href="/team" className="hover:text-primary">TEAM</Link></li>
     {!mobile && <li className="h-4 w-px bg-gray-300"></li>}
     <li><Link href="/investor-relations" className="hover:text-primary">INVESTOR RELATIONS</Link></li>
-    {!mobile && <li className="h-4 w-px bg-gray-300"></li>}
   </>
 )
 
